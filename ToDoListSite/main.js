5e9baf408291f561f87d8dbcc98dd6c5d3b06809
ONKJUNUA <jaseko2002@gmail.com>
2023-03-06 19:01:28 +0100

    delete function works

--- ToDoListSite/main.js
+++ ToDoListSite/main.js
@@ -1,10 +1,13 @@
-const todos = [{
+let todos = [{
     title: 'Make some code',
-    dueDate: '2022-12-01'},{
+    dueDate: '2022-12-01',
+    id: 'id1'},{
     title: 'Go out with dog',
-    dueDate: '2021-03-02'},{
+    dueDate: '2021-03-02',
+    id: 'id2'},{
     title: 'Eat some meat',
-    dueDate: '2022-05-25'},];
+    dueDate: '2022-05-25',
+    id: 'id3'}];
 
 render();
 
@@ -14,15 +17,29 @@ function addToDo(){
 
     const datePicker = document.getElementById('date-picker');
     const dueDate = datePicker.value;
+
+    const id = '' + new Date().getTime();
+
+
     todos.push({
         title: title,
-        dueDate:dueDate
+        dueDate:dueDate,
+        id: id
     });
     render();
 }
 
-function deleteToDo(){
-    
+function deleteToDo(event){
+    const deleteButton = event.target;
+    const idToDelete = deleteButton.id;
+    todos = todos.filter(function (todo) {
+        if(todo.id===idToDelete){
+            return false;
+        } else {
+            return true;
+        }
+    });
+    render();
 }
 
 function render(){
@@ -34,6 +51,7 @@ function render(){
         const deleteButton = document.createElement('button');
         deleteButton.innerText = 'Delete';
         deleteButton.onclick = deleteToDo;
+        deleteButton.id = todo.id;
         element.appendChild(deleteButton);
 
         const todoList = document.getElementById('todo-list');
